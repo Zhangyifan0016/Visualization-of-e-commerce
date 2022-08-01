e0ea41ed08bba72744d6026e0b49be15e492ac78
Zhangyifan0016 <z351072259@163.com>
2022-08-01 16:25:49 +0800

    库存与销量模块(圆环饼图)

---
+++ visual/src/components/Stock.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="com-container">
+    <div class="com-chart" ref="stockRef"></div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null, // 服务器返回的数据
+      timer: null,
+      currentIndex: 0
+    }
+  },
+  mounted() {
+    this.initChart()
+    this.getData()
+    this.screenAdapter()
+    window.addEventListener('resize', this.screenAdapter)
+  },
+  destroyed() {
+    clearInterval(this.timer)
+    window.removeEventListener('resize', this.screenAdapter)
+  },
+
+  methods: {
+    initChart() {
+      this.chartInstance = this.$echarts.init(this.$refs.stockRef, 'chalk')
+      // 初始化配置控制
+      const initOption = {
+        title: {
+          text: '▎库存和销量分析',
+          left: 20,
+          top: 20
+        }
+      }
+      this.chartInstance.setOption(initOption)
+
+      this.chartInstance.on('mouseover', () => {
+        clearInterval(this.timer)
+      })
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
+    },
+    async getData() {
+      const { data: res } = await this.$http.get('stock')
+      this.allData = res
+      this.updateChart()
+      this.startInterval()
+    },
+    updateChart() {
+      const centerArr = [
+        ['18%', '40%'],
+        ['50%', '40%'],
+        ['82%', '40%'],
+        ['34%', '75%'],
+        ['66%', '75%']
+      ]
+
+      const colorArr = [
+        ['#4FF778', '#0BA82C'],
+        ['#E5DD45', '#E8B11C'],
+        ['#E8821C', '#E55445'],
+        ['#5052EE', '#AB6EE5'],
+        ['#23E5E5', '#2E72BF']
+      ]
+      const start = this.currentIndex * 5
+      const end = (this.currentIndex + 1) * 5
+      // 包左不包右
+      const showData = this.allData.slice(start, end)
+      const sellerArr = showData.map((item, index) => {
+        return {
+          type: 'pie',
+          radius: [110, 100],
+          center: centerArr[index],
+          hoverAnimation: false,
+          labelLine: {
+            show: false
+          },
+          label: {
+            position: 'center',
+            color: colorArr[index][0]
+          },
+          data: [
+            {
+              name: item.name + '\n' + item.sales,
+              value: item.sales,
+              itemStyle: {
+                color: new this.$echarts.graphic.LinearGradient(0, 1, 0, 0, [
+                  {
+                    offset: 0,
+                    color: colorArr[index][0]
+                  },
+                  {
+                    offset: 1,
+                    color: colorArr[index][1]
+                  }
+                ])
+              }
+            },
+            {
+              value: item.stock,
+              itemStyle: {
+                color: '#333843'
+              }
+            }
+          ]
+        }
+      })
+      const dataOption = {
+        series: sellerArr
+      }
+      this.chartInstance.setOption(dataOption)
+    },
+    startInterval() {
+      if (this.timer) {
+        clearInterval(this.timer)
+      }
+      this.timer = setInterval(() => {
+        this.currentIndex++
+        if (this.currentIndex > 1) {
+          this.currentIndex = 0
+        }
+        this.updateChart()
+      }, 5000)
+    },
+    // 屏幕适配
+    screenAdapter() {
+      const titleFontSize = (this.$refs.stockRef.offsetWidth / 100) * 3.6
+      const innerRadius = titleFontSize * 2
+      const outterRadius = innerRadius * 1.125
+      // 分辨率配置项
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize
+          }
+        },
+        series: [
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          },
+          {
+            type: 'pie',
+            radius: [outterRadius, innerRadius],
+            label: {
+              fontSize: titleFontSize / 2
+            }
+          }
+        ]
+      }
+      this.chartInstance.setOption(adapterOption)
+      // 手动调用图表resize方法
+      this.chartInstance.resize()
+    }
+  }
+}
+</script>
+<style scoped lang="scss"></style>
